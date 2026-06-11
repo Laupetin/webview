661fafd4e70a77816ebe074ff2d587ff332aaafe
Jan Laupetin <jan@laupetin.net>
2026-06-11 20:08:45 +0200

    chore: adjust header guards in platform

--- include/webview/detail/platform/linux/gtk_compat.hpp
+++ include/webview/detail/platform/linux/gtk_compat.hpp
@@ -28,7 +28,7 @@
 #ifndef WEBVIEW_PLATFORM_LINUX_GTK_COMPAT_HPP
 #define WEBVIEW_PLATFORM_LINUX_GTK_COMPAT_HPP
 
-#include "../../../macros.hpp"
+#include "../../macros.hpp"
 
 #if defined(WEBVIEW_PLATFORM_LINUX) && defined(WEBVIEW_GTK)
 
