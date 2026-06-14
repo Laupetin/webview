c353d6bd6d0e944f3160943d55e9cddef12d1d56
Jan Laupetin <jan@laupetin.net>
2026-06-15 00:24:22 +0200

    feat: add title handler plugin

---
+++ examples/title_handler_plugin.cpp
@@ -0,0 +1,79 @@
+#include "webview/plugin/title_handler.hpp"
+#include "webview/webview.hpp"
+
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <thread>
+
+constexpr auto html =
+    R"html(
+<div>
+  <input id="titleInput" type="text" placeholder="Enter new title" />
+</div>
+<hr />
+<div>
+  <button id="action">Set title</button>
+</div>
+
+<style>
+body {
+  background: #e3e3e3;
+  color: #000000;
+}
+@media (prefers-color-scheme: dark) {
+  body {
+    background: #1c1c1c;
+    color: #ffffff;
+  }
+}
+</style>
+
+<script type="module">
+  const getElements = ids => Object.assign({}, ...ids.map(
+    id => ({ [id]: document.getElementById(id) })));
+  const ui = getElements([
+    "action", "titleInput"
+  ]);
+  ui.action.addEventListener("click", async () => {
+    document.title = ui.titleInput.value;
+  });
+</script>
+)html";
+
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE /*hInst*/, HINSTANCE /*hPrevInst*/, LPSTR /*lpCmdLine*/, int /*nCmdShow*/)
+{
+#else
+int main()
+{
+#endif
+  long count = 0;
+
+  auto w = std::make_unique<webview::window>();
+  w->set_debug(true);
+  w->set_title("Title handler example");
+  w->set_window_min(200, 200);
+  w->set_window_max(600, 600);
+  w->set_window_size(480, 320);
+
+  auto result = w->set_html(html);
+  if (!result.has_value())
+  {
+    std::cerr << "Failed to set html: " << result.error().message() << std::endl;
+    return 1;
+  }
+
+  webview::app app;
+
+  app.register_plugin(std::make_shared<webview::title_handler_plugin>());
+
+  result = app.run(std::move(w));
+  if (!result.has_value())
+  {
+    std::cerr << "Failed to run app: " << result.error().message() << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
---
+++ include/webview/plugin/detail/title_handler_windows.hpp
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#ifndef WEBVIEW_PLUGIN_DETAIL_TITLE_HANDLER_WINDOWS_HPP
+#define WEBVIEW_PLUGIN_DETAIL_TITLE_HANDLER_WINDOWS_HPP
+
+#include "../../detail/macros.hpp"
+#include "../title_handler.hpp"
+
+#if defined(WEBVIEW_PLATFORM_WINDOWS) && defined(WEBVIEW_EDGE)
+#ifdef WEBVIEW_INCLUDE_IMPL
+
+#include "../../detail/platform/windows/webview2/loader.hpp"
+
+#include <Windows.h>
+#include <wrl/event.h>
+
+namespace webview
+{
+  WEBVIEW_IMPL std::expected<void, std::string> title_handler_plugin::on_setup_window(window& window, const plugin_window_context& context)
+  {
+    const auto maybe_browser_controller = context.browser_controller();
+    if (!maybe_browser_controller.has_value())
+      return std::unexpected("Failed to get browser controller");
+    const auto controller = static_cast<ICoreWebView2Controller*>(maybe_browser_controller.value());
+
+    const auto maybe_window = context.window();
+    if (!maybe_window.has_value())
+      return std::unexpected("Failed to get window");
+    auto window_hwnd = static_cast<HWND>(maybe_window.value());
+
+    Microsoft::WRL::ComPtr<ICoreWebView2> core;
+    if (FAILED(controller->get_CoreWebView2(&core)))
+      return std::unexpected("Failed to get webview");
+
+    EventRegistrationToken token;
+    if (FAILED(core->add_DocumentTitleChanged(Microsoft::WRL::Callback<ICoreWebView2DocumentTitleChangedEventHandler>(
+                                                  [window_hwnd](ICoreWebView2* sender, IUnknown* args) -> HRESULT
+                                                  {
+                                                    LPWSTR title;
+
+                                                    if (FAILED(sender->get_DocumentTitle(&title)))
+                                                      return S_FALSE;
+
+                                                    SetWindowTextW(window_hwnd, title);
+                                                    CoTaskMemFree(title);
+
+                                                    return S_OK;
+                                                  })
+                                                  .Get(),
+                                              &token)))
+    {
+      return std::unexpected("Failed to add title handler");
+    }
+
+    return {};
+  }
+} // namespace webview
+
+#endif
+#endif
+#endif
